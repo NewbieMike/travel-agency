88089c0bc3a20f339d422a99beb31735b1605fdd
Michał Micherda <mmicherdakr@gmail.com>
2021-05-01 14:36:39 +0200

    duration and tags bugs

--- src/components/features/TripListOptions/TripListOptions.js
+++ src/components/features/TripListOptions/TripListOptions.js
@@ -9,15 +9,18 @@ class TripListOptions extends React.Component {
     if(checked) {
       console.log('Adding tag', tag);
       // TODO - use action dispatcher from props
+      this.props.changeSearchTag({tag: tag});
     } else {
       console.log('Removing tag', tag);
       // TODO - use action dispatcher from props
+      //this.props.changeSearchTag({tag: tag});
     }
   }
 
   handleDuration(type, value){
     console.log('Changing duration', type, value);
     // TODO - use action dispatcher from props
+    this.props.changeSearchDuration({type: type, value: value});
   }
 
   handleSearch(phrase){
@@ -73,6 +76,8 @@ TripListOptions.propTypes = {
   tags: PropTypes.object,
   filters: PropTypes.object,
   changeSearchPhrase: PropTypes.func,
+  changeSearchTag: PropTypes.func,
+  changeSearchDuration: PropTypes.func,
 };
 
 export default TripListOptions;
--- src/redux/tripsRedux.js
+++ src/redux/tripsRedux.js
@@ -12,28 +12,40 @@ export const getFilteredTrips = ({trips, filters}) => {
   }
 
   // TODO - filter by duration
-
+  if(filters.duration){
+    output = output.filter(trip => 
+      trip.days >= parseInt(filters.duration.from) && trip.days <= parseInt(filters.duration.to)
+    );
+  }
   // TODO - filter by tags
-
+  if(filters.tags){
+    for(let tag of filters.tags){
+      output = output.filter(trip => trip.tags.contains(tag));
+    } 
+  }
   // TODO - sort by cost descending (most expensive goes first)
 
   return output;
 };
 
 export const getTripById = ({trips}, tripId) => {
-  const filtered = trips;
+  //const filtered = trips;
 
   // TODO - filter trips by tripId
-
+  const filtered = trips.filter(
+    trip => trip.id == tripId
+  );
   console.log('filtering trips by tripId:', tripId, filtered);
   return filtered.length ? filtered[0] : {error: true};
 };
 
 export const getTripsForCountry = ({trips}, countryCode) => {
-  const filtered = trips;
+  //const filtered = trips;
 
   // TODO - filter trips by countryCode
-
+  const filtered = trips.filter(
+    trip => trip.country.code == countryCode
+  );
   console.log('filtering trips by countryCode:', countryCode, filtered);
   return filtered.length ? filtered : [{error: true}];
 };
